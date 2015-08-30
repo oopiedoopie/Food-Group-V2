e76e66a9ee3c69cb5d85ce8167c16513d903e2c1
Jay <oopiedoopie@oopies-MacBook-Pro.local>
2015-08-30 17:21:50 -0400

    Deleted Unnecessary viewControllers
    Preparing to add new screens and pageView. Deleted unnecessary
    viewControllers from StoryBoard.

---
+++ Food Group/ContentViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ContentViewController.swift
+//  Food Group
+//
+//  Created by oopie doopie on 8/24/15.
+//  Copyright (c) 2015 Oopie Doopie. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ContentViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
+
