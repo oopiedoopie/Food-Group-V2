62d70f0693c11569f3f9ae99c334d41c991c03e1
Jay <oopiedoopie@oopies-MacBook-Pro.local>
2015-08-30 19:48:02 -0400

    GitHub Fixes Attempt 1
    Attempt 1

--- Food Group/ResultsViewController.swift
+++ Food Group/ResultsViewController.swift
@@ -15,7 +15,16 @@ class ResultsViewController: UIViewController, PNChartDelegate{
     
     override func viewDidLoad() {
         super.viewDidLoad()
+<<<<<<< HEAD
         
+=======
+        let chartSize = self.barChart.frame.size
+        let chartWidth = chartSize.width
+        let chartHeight = chartSize.height
+        let screenSize: CGRect = UIScreen.mainScreen().bounds
+        let screenWidth = screenSize.width
+        let screenHeight = screenSize.height
+>>>>>>> parent of b1ed0e1... small changes
         barChart.xLabels = ["Brixx Pizza", "El Jalisco", "La Parilla", "Zaxbys", "Chick-fil-A", "Pizza Inn"]
         barChart.yValues = [5, 2, 3, 1, 1, 1]
         barChart.isShowNumbers = true
