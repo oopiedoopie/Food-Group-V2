d7e0d36797be60273287e448328f9d7f64a46d32
Eric Cauble <caublee@email.uscupstate.edu>
2015-08-30 19:33:50 -0400

    Chart test

--- Food Group/ResultsViewController.swift
+++ Food Group/ResultsViewController.swift
@@ -9,25 +9,24 @@
 import UIKit
 import PNChart
 
-class ResultsViewController: UIViewController{
+class ResultsViewController: UIViewController, PNChartDelegate{
     
     @IBOutlet weak var barChart: PNBarChart!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let chartSize = self.barChart.frame.size
-        let chartWidth = chartSize.width
-        let chartHeight = chartSize.height
-        println(chartWidth)
-        println(chartHeight)
-        let screenSize: CGRect = UIScreen.mainScreen().bounds
-        let screenWidth = screenSize.width
-        let screenHeight = screenSize.height
+        
         barChart.xLabels = ["Brixx Pizza", "El Jalisco", "La Parilla", "Zaxbys", "Chick-fil-A", "Pizza Inn"]
         barChart.yValues = [5, 2, 3, 1, 1, 1]
         barChart.isShowNumbers = true
+        barChart.sizeToFit()
         barChart.strokeChart()
         barChart.barBackgroundColor = UIColor.orangeColor()
+        barChart.strokeChart()
+        barChart.delegate = self
+        self.view.addSubview(barChart)
+
+
     }
     
     
