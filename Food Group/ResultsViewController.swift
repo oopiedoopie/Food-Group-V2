73797f1ffd0e01345298a01e07f110e39d75bd1e
Eric Cauble <caublee@email.uscupstate.edu>
2015-08-31 18:19:54 -0400

    Trying to merge fixed with master

--- Food Group/ResultsViewController.swift
+++ Food Group/ResultsViewController.swift
@@ -15,35 +15,20 @@ class ResultsViewController: UIViewController, PNChartDelegate{
     
     override func viewDidLoad() {
         super.viewDidLoad()
-<<<<<<< HEAD
         barChart.delegate = self
-=======
-<<<<<<< Updated upstream
-<<<<<<< HEAD
-        
-=======
->>>>>>> master
         let chartSize = self.barChart.frame.size
         let chartWidth = chartSize.width
         let chartHeight = chartSize.height
+        println(chartWidth)
+        println(chartHeight)
         let screenSize: CGRect = UIScreen.mainScreen().bounds
         let screenWidth = screenSize.width
         let screenHeight = screenSize.height
->>>>>>> parent of b1ed0e1... small changes
-=======
-        
->>>>>>> Stashed changes
         barChart.xLabels = ["Brixx Pizza", "El Jalisco", "La Parilla", "Zaxbys", "Chick-fil-A", "Pizza Inn"]
         barChart.yValues = [5, 2, 3, 1, 1, 1]
         barChart.isShowNumbers = true
-        barChart.sizeToFit()
         barChart.strokeChart()
         barChart.barBackgroundColor = UIColor.orangeColor()
-        barChart.strokeChart()
-        barChart.delegate = self
-        self.view.addSubview(barChart)
-
-
     }
     
     
